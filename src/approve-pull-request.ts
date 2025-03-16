2f085824da305c78f26a2cfe951feb03bd36f40f
brenorodriguesdev <brenorodriguesdev@gmail.com>
2025-03-16 16:50:18 -0300

    fix: approve - deny

--- src/approve-pull-request.ts
+++ src/approve-pull-request.ts
@@ -3,10 +3,19 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 (async () => {
-  const url = `https://api.github.com/repos/${process.env.REPO_NAME}/pulls/${process.env.PR_NUMBER}/reviews`;
+
+  const urlRequestReview = `https://api.github.com/repos/${process.env.REPO_NAME}/pulls/${process.env.PR_NUMBER}/requested_reviewers`;
+
   await axios.post(
-    url,
-    { event: "APPROVE", body: "✅ Testes E2E aprovados! PR aprovado automaticamente." },
+    urlRequestReview,
+    { reviewers: ["brenorodriguesdev"] },
+    { headers: { Authorization: `token ${process.env.GITHUB_TOKEN}`, Accept: "application/vnd.github.v3+json" } }
+  );
+
+  const urlApproveReview = `https://api.github.com/repos/${process.env.REPO_NAME}/pulls/${process.env.PR_NUMBER}/reviews`;
+  await axios.post(
+    urlApproveReview,
+    { reviewers: ["brenorodriguesdev"], event: "APPROVE", body: "✅ Testes E2E aprovados! PR aprovado automaticamente." },
     { headers: { Authorization: `token ${process.env.GITHUB_TOKEN}`, Accept: "application/vnd.github.v3+json" } }
   );
 })();
